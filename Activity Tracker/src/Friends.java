dbcc1ce1c100ee8672bfa29a28b592af50c69847
MikeCodeGuru <ma6166@mun.ca>
2018-10-30 15:57:02 -0230

    So, these are the separate classes stored in the UserApp class

---
+++ Activity Tracker/src/Friends.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Friends{
+		
+		private boolean viewUserList;
+		private boolean viewFriendsList;
+		private boolean viewProfile;
+		private boolean sendRequest;
+		private boolean acceptRequest;
+		private boolean denyRequest;
+		
+		public Friends(String firstName, String lastName, String fullName, String password,
+				ArrayList<String> friendsList, ArrayList<String> sessionList, ArrayList<String> deviceList) 
+		{
+			
+		}
+		
+	}
---
+++ Activity Tracker/src/Session.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Session{
+		
+		private float calories;
+		private float heartRate;
+		private float aveSpeed;
+		private float dehydration;
+		private float cumDistance;
+		private String date;
+		
+		public Session(String firstName, String lastName, String fullName, String password, ArrayList<String> friendsList,
+				ArrayList<String> sessionList, ArrayList<String> deviceList) {
+			// TODO Auto-generated constructor stub
+		}
+		
+	}
